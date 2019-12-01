e1d4ff8d3af34414de9b1ad8e1a4e38968500a02
Adrià Martí Blasco <adria.marti.blasco@gmail.com>
2019-12-01 17:42:26 +0100

    test: write some solidity tests

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -7,6 +7,29 @@ import "../contracts/EventTicketsV2.sol";
 
 contract TestEventTicket {
 
+  string DESCRIPTION = "description";
+  string URL = "URL";
+  uint TICKET_NUMBER = 100;
 
+  function testSalesAreOpenWhenContractIsCreated() public {
+    EventTickets eventTickets = new EventTickets(DESCRIPTION, URL, TICKET_NUMBER);
+
+    (, , , , bool isOpen) = eventTickets.readEvent();
+
+    Assert.equal(isOpen, true, "the event should be open");
+  }
+
+  function testReadEventFunction() public {
+    EventTickets eventTickets = new EventTickets(DESCRIPTION, URL, TICKET_NUMBER);
+
+    (string memory description, string memory website, uint totalTickets, uint sales, bool isOpen) = eventTickets.readEvent();
+    
+
+    Assert.equal(description, DESCRIPTION, "the event descriptions should match");
+    Assert.equal(website, URL, "the event urls should match");
+    Assert.equal(totalTickets, TICKET_NUMBER, "the number of tickets for sale should be set");
+    Assert.equal(sales, 0, "the ticket sales should be 0");
+    Assert.equal(isOpen, true, "the event should be open");
+  }
 
 }
